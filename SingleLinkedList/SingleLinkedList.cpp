79011afc3daf6f923a7a6ab45332a343e71e2553
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-26 11:46:50 +0700

    pilihan 5: keluar

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -179,3 +179,17 @@ int main() {
 			case 4:
 				searchData();
 				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi Kesalahan" << endl;
+		}
+
+	} while (pilihan != 5);
+}
