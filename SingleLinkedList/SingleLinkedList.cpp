d5a2526a719318ef98a189e03bb9cc81d3a37751
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-26 11:39:46 +0700

    prosedur menghapus data node

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -72,3 +72,14 @@ bool searchNode(int nim, Node* current, Node* previous) {
 		return false;
 	}
 }
+
+bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (searchNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
