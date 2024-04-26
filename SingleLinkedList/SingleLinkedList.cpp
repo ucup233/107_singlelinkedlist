5f18baf31010d5d770b90a122d8b46cfadd54a2d
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-26 11:45:29 +0700

    case 2: pilihan untuk hapus data

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -153,3 +153,23 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
