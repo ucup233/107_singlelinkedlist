1833cd91a39e4afa05b25e00a2886aff765fe0c3
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-04-26 11:42:08 +0700

    prosedur mencari isi data node

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -115,3 +115,18 @@ void searchData() {
 		system("cls");
 		return;
 	}
+	else {
+		int nim;
+		cout << "Masukkan NIM: ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM: " << currentNode->noMhs << ",Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
+	}
+}
